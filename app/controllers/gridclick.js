f20eed89c0168a567b5fd47a1deddaa9a5fe691e
DesaiSamir <desaisamirv@yahoo.com>
2014-04-26 11:00:21 -0400

    added new code

--- app/controllers/gridclick.js
+++ app/controllers/gridclick.js
@@ -1,4 +1,33 @@
-$(document).ready(function (){
+$(document).ready(function () {
+    var isMobile = {
+        Android: function () {
+            return navigator.userAgent.match(/Android/i);
+        },
+        BlackBerry: function () {
+            return navigator.userAgent.match(/BlackBerry/i);
+        },
+        iOS: function () {
+            return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+        },
+        Opera: function () {
+            return navigator.userAgent.match(/Opera Mini/i);
+        },
+        Windows: function () {
+            return navigator.userAgent.match(/IEMobile/i);
+        },
+        any: function () {
+            return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+        }
+    };
+    
+    window.addEventListener("load", function () {
+        // Set a timeout...
+        setTimeout(function () {
+            // Hide the address bar!
+            window.scrollTo(0, 1);
+        }, 0);
+    });
+    
    $('.ui-btnsolid').click(function (){
            $(this).toggleClass('ui-btnsolid').toggleClass('ui-btntranspo');
 		   
